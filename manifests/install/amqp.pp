ea944d05f43acb9b305767c684c0fecb4ac83f12
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-07-01 13:41:46 -0400

    fix?

--- manifests/install/amqp.pp
+++ manifests/install/amqp.pp
@@ -1,7 +1,7 @@
 class carbon::install::amqp (
   $amqp_pip_pkgs = $::carbon_amqp_pip_pkgs
   ) {
-  $amqp_pip_pkgs.each |String pkg| {
+  $amqp_pip_pkgs.each |String $pkg| {
     python::pip { $pkg: }
   }
 }
