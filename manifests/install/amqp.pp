3bf1dc8ce7ba6982b14d9ea1554664f1cb4bc07b
Matthew Nicholson <matthew.a.nicholson@gmail.com>
2016-07-01 13:46:44 -0400

    try try

--- manifests/install/amqp.pp
+++ manifests/install/amqp.pp
@@ -2,6 +2,9 @@ class carbon::install::amqp (
   $amqp_pip_pkgs = $::carbon_amqp_pip_pkgs
   ) {
   $amqp_pip_pkgs.each |String $pkg| {
-    python::pip { $pkg: }
+    python::pip { $pkg:
+      pkgname => $pkg,
+      ensure  => present,
+    }
   }
 }
